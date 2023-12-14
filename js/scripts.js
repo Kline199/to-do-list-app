15302be5ea1bb67f7586ddf1befe6065487d5db4
Kline199 <seankline1@gmail.com>
2023-12-13 22:38:47 -0600

    Fixed noted issues.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
-function newItem(event) {
-    event.preventDefault();
-  
+function newItem() {
+    // Adding a new item to the list of items:
     let li = $('<li></li>');
     let inputValue = $('#input').val();
     li.append(inputValue);
@@ -11,21 +10,26 @@ function newItem(event) {
       $('#list').append(li);
     }
   
+    // Crossing out an item from the list of items:
     li.on("dblclick", function () {
       li.toggleClass("strike");
     });
   
-    let crossOutButton = $('<button>X</button>');
+    // Adding the delete button "X":
+    let crossOutButton = $('<crossOutButton>X</crossOutButton>');
     li.append(crossOutButton);
   
     crossOutButton.on("click", function () {
       li.addClass("delete");
     });
   
+    // Clear the input field after adding an item
     $('#input').val('');
   
+    // Reordering the items:
     $('#list').sortable();
   }
   
+  // Bind the function to the form submit event
   $('form[name="toDoList"]').submit(newItem);
   
